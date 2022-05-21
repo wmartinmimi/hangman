b3d5783a779b36343eec4267e0a9a6b30126cf72
wmartinmimi <martinmimiw@gmail.com>
2022-05-21 20:35:40 +0100

    updated

--- hangman.py
+++ hangman.py
@@ -1,6 +1,79 @@
 import random
 import getpass
 
+hangman = [
+  """
+  ----
+  |  o
+  | /|\\
+  | / \\
+  -----
+  """,
+  """
+  ----
+  |  o
+  | /|\\
+  | /
+  -----
+  """,
+  """
+  ----
+  |  o
+  | /|\\
+  |
+  -----
+  """,
+  """
+  ----
+  |  o
+  | /|
+  |
+  -----
+  """,
+  """
+  ----
+  |  o
+  |  |
+  |
+  -----
+  """,
+  """
+  ----
+  |  o
+  |
+  |
+  -----
+  """,
+  """
+  ----
+  |
+  |
+  |
+  -----
+  """,
+  """
+  
+  |
+  |
+  |
+  -----
+  """,
+  """
+  
+  
+  
+  
+  -----
+  """,
+  """
+  
+  
+  
+  
+  
+  """
+]
+
 word_list = ['apples', 'bananas', 'oranges']
 
 print('enter word to be guessed')
@@ -9,6 +82,7 @@ if word == '':
   word = word_list[random.randint(0, len(word_list) - 1)]
 characters = list(word)
 show = characters.copy()
+used = []
 
 for i in range(len(show)):
   if show[i] != ' ':
@@ -17,12 +91,19 @@ for i in range(len(show)):
 lives = 9
 won = False
 while not won and lives > 0:
+  print(hangman[lives])
   result = ''
   for char in show:
     result += char
   print(f'{result} lives: {lives}')
+  result = ''
+  for char in used:
+    result += char + ' '
+  print(result)
   print('enter guess')
   guess = input('> ')
+  if guess == '':
+    continue
   correct = False
   won = True
   for i in range(len(show)):
@@ -33,9 +114,16 @@ while not won and lives > 0:
       won = False
   if not correct:
     lives -= 1
+    used.append(guess)
+
+print()
 if won:
+  print('congraduations,')
   print('you won! :)')
 else:
+  print('oh no,')
   print('you lost... :(')
+print(hangman[lives])
+print(f'incorrect guesses: {len(used)}')
 print(f'the word is {word}.')
 print('made by martinmimi.')
