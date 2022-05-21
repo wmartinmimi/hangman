be85c3e496f743687cb8b2f0f1ff13145c2cd8a4
wmartinmimi <martinmimiw@gmail.com>
2022-05-21 21:01:13 +0100

    added comments

--- hangman.py
+++ hangman.py
@@ -1,6 +1,7 @@
 import random
 import getpass
 
+# images
 hangman = [
   """
   ----
@@ -74,12 +75,16 @@ hangman = [
   """
 ]
 
+# some default words
 word_list = ['apples', 'bananas', 'oranges']
 
+# word prompt
 print('enter word to be guessed')
 word = getpass.getpass('> ')
 if word == '':
   word = word_list[random.randint(0, len(word_list) - 1)]
+
+# game setup
 characters = list(word)
 show = characters.copy()
 used = []
@@ -88,26 +93,35 @@ for i in range(len(show)):
   if show[i] != ' ':
     show[i] = '_'
 
+# game logic
 lives = 9
 won = False
 while not won and lives > 0:
+  # output hangman image
   print(hangman[lives])
   result = ''
   for char in show:
     result += char
   print(f'{result} lives: {lives}')
+
+  # output used characters
   result = ''
   for char in used:
     result += char + ' '
   print(result)
+
+  # prompt for guess
   print('enter guess')
   guess = input('> ')
+
   if len(guess) != 1:
     print('1 letter guess only!')
     continue
   if guess in used or guess in show:
     print('guessed already!')
     continue
+
+  # guess logic
   correct = False
   won = True
   for i in range(len(show)):
@@ -121,6 +135,7 @@ while not won and lives > 0:
     used.append(guess)
     used = sorted(used)
 
+# ui
 print()
 if won:
   print('congraduations,')
@@ -131,4 +146,6 @@ else:
 print(hangman[lives])
 print(f'incorrect guesses: {len(used)}')
 print(f'the word is {word}.')
+
+# credits
 print('made by martinmimi.')
